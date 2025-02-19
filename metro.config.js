74de4461d5f4d55da06fa028239da4965be439aa
nikunj <nikunjramani567@gmail.com>
2025-02-19 15:08:56 +0530

    change in login and logged in functionality is change

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration
+//  * https://facebook.github.io/metro/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const customConfig = {
+//   maxWorkers: 8,
+//   resolver: {
+//     sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'], // Ensure extensions are correct
+//   },
+// };
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), customConfig);
+
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
 
 /**
  * Metro configuration
@@ -10,7 +29,8 @@ const customConfig = {
   maxWorkers: 8,
   resolver: {
     sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'], // Ensure extensions are correct
+    assetExts: [...defaultConfig.resolver.assetExts, 'lottie'], // Merge asset extensions
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), customConfig);
+module.exports = mergeConfig(defaultConfig, customConfig);
--- src/screen/auth/SplashScreen.js
+++ src/screen/auth/SplashScreen.js
@@ -18,17 +18,23 @@ const SplashScreen = () => {
     setVisible(true);
     const user = await AsyncStorage.getItem('user');
     setVisible(false);
-    LottieSplashScreen.hide();
+
     if (user) {
-      navigation.reset({
-        index: 0,
-        routes: [{name: ScreenName.home}],
-      });
+      setTimeout(() => {
+        LottieSplashScreen.hide();
+        navigation.reset({
+          index: 0,
+          routes: [{name: ScreenName.home}],
+        });
+      }, 4500);
     } else {
-      navigation.reset({
-        index: 0,
-        routes: [{name: ScreenName.signIn}],
-      });
+      setTimeout(() => {
+        LottieSplashScreen.hide();
+        navigation.reset({
+          index: 0,
+          routes: [{name: ScreenName.signIn}],
+        });
+      }, 4500);
     }
   }, [navigation]);
 
@@ -46,66 +52,3 @@ const styles = StyleSheet.create({
   },
   lottie: {width: '100%', height: '100%'},
 });
-
-// import React, {useCallback, useEffect, useState} from 'react';
-// import {View, StyleSheet} from 'react-native';
-// import LottieView from 'lottie-react-native';
-// import {useNavigation} from '@react-navigation/native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import {ScreenName} from '../../component/Screen';
-// import LottieSplashScreen from 'react-native-lottie-splash-screen';
-
-// const SplashScreen = () => {
-//   const navigation = useNavigation();
-//   const [animationFinished, setAnimationFinished] = useState(false);
-//   const [user, setUser] = useState('');
-
-//   useEffect(() => {
-//     checkLogin();
-//   }, [checkLogin]);
-
-//   const checkLogin = useCallback(async () => {
-//     const userData = await AsyncStorage.getItem('user');
-//     setUser(userData);
-
-//     // Wait for splash screen to close before navigating
-//     setTimeout(() => {
-//       LottieSplashScreen.hide();
-//       setAnimationFinished(true);
-//     }, 2000);
-//   }, []);
-
-//   useEffect(() => {
-//     if (animationFinished) {
-//       // Navigate only after animation is finished
-//       navigation.reset({
-//         index: 0,
-//         routes: [{name: user ? ScreenName.home : ScreenName.signIn}],
-//       });
-//     }
-//   }, [animationFinished, navigation, user]);
-
-//   return (
-//     <View style={styles.container}>
-//       <LottieView
-//         source={require('../../Assets/JsonFile/splashscreen.json')}
-//         autoPlay
-//         loop={false} // Stops looping so it finishes
-//         onAnimationFinish={() => setAnimationFinished(true)}
-//         style={styles.lottie}
-//       />
-//     </View>
-//   );
-// };
-
-// export default SplashScreen;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#FFFFFF',
-//   },
-//   lottie: {width: '100%', height: '100%'},
-// });
